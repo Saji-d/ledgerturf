7075b25cde800e7ec13a479b5faa474b2ac573a3
Sajidur Rahman Sajid <sajidsajidurrahman99@gmail.com>
2026-05-16 17:09:38 +0600

    Fixed Vercel conflicts

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:3010',
-      'http://localhost:3000',
-    ],
+    origin: '*',
     credentials: true,
   })
 );
